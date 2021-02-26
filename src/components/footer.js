c1d6deb1c52cc60331e7e4afb47d921adc18d1b0
hiredtocode <jasonhan@kakao.com>
2021-02-26 14:10:04 +0900

    footer area fixed to bottom

--- src/components/footer.js
+++ src/components/footer.js
@@ -22,12 +22,8 @@ const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
     flexDirection: 'column',
-    minHeight: '100vh',
   },
-  main: {
-    marginTop: theme.spacing(8),
-    marginBottom: theme.spacing(2),
-  },
+
   footer: {
     padding: theme.spacing(3, 2),
     marginTop: 'auto',
@@ -44,16 +40,6 @@ export default function StickyFooter() {
   return (
     <div className={classes.root}>
       <CssBaseline />
-      <Container component="main" className={classes.main} maxWidth="sm">
-        <Typography variant="h2" component="h1" gutterBottom>
-          Sticky footer
-        </Typography>
-        <Typography variant="h5" component="h2" gutterBottom>
-          {'Pin a footer to the bottom of the viewport.'}
-          {'The footer will move as the main element of the page grows.'}
-        </Typography>
-        <Typography variant="body1">Sticky footer placeholder.</Typography>
-      </Container>
       <footer className={classes.footer}>
         <Container maxWidth="sm">
           <Typography variant="body1">
