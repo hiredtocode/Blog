5d962f0990a9e390ea876111e4da28d4ffb2b3a7
hiredtocode <jasonhan@kakao.com>
2021-03-01 11:24:50 +0900

    Splitted sidebar files into two different ones to fix sidebar not opening when in larger window

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,93 +1,18 @@
+/* eslint-disable no-nested-ternary */
 import React from 'react';
 import {
-  Drawer,
-  AppBar,
-  Toolbar,
-  Button,
   CssBaseline,
-  Typography,
-  IconButton,
   makeStyles,
   useTheme,
   useMediaQuery,
 } from '@material-ui/core';
-import MenuIcon from '@material-ui/icons/Menu';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import { Link } from 'gatsby-theme-material-ui';
-import clsx from 'clsx';
-import Header from './header';
-import Nav from './nav';
-import Sidebar2 from './sidebar2';
 
-const drawerWidth = 240;
+import SidebarClosed from './sidebarClosed';
+import SidebarOpen from './sidebarOpen';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
   root: {
     display: 'flex',
-  },
-  appBar: {
-    zIndex: theme.zIndex.drawer + 1,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-  },
-  appBarShift: {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  menuButton: {
-    marginRight: 36,
-  },
-  hide: {
-    display: 'none',
-  },
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0,
-    whiteSpace: 'nowrap',
-  },
-  drawerOpen: {
-    width: drawerWidth,
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  drawerClose: {
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    overflowX: 'hidden',
-    width: theme.spacing(7) + 1,
-  },
-  header: {
-    flexGrow: 1,
-  },
-  toolbar: {
-    display: 'flex',
-    alignItems: 'center',
-    paddingLeft: 18,
-    paddingRight: 24,
-
-    justifyContent: 'flex-end',
-    [theme.breakpoints.down('xs')]: {
-      minHeight: 64,
-      paddingLeft: 18,
-      paddingRight: 24,
-    },
-    // necessary for content to be below app bar
-    ...theme.mixins.toolbar,
-  },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing(3),
   },
 }));
 
@@ -95,76 +20,24 @@ export default function Sidebar({ children }) {
   const classes = useStyles();
   const theme = useTheme();
 
-  const [open, setOpen] = React.useState(true);
-
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
-
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
-  const isMatch = useMediaQuery(theme.breakpoints.down('sm'));
+  const isSmall = useMediaQuery(theme.breakpoints.down('sm'));
+  const isLarge = useMediaQuery(theme.breakpoints.up('sm'));
 
   return (
     // <ThemeProvider theme={custom}>
     <div className={classes.root}>
       <CssBaseline />
-      {isMatch ? (
-        <Sidebar2 />
+      {isSmall ? (
+        <>
+          <SidebarClosed />
+        </>
+      ) : isLarge ? (
+        <>
+          <SidebarOpen />
+        </>
       ) : (
         <>
-          {' '}
-          <AppBar
-            position="fixed"
-            className={clsx(classes.appBar, {
-              [classes.appBarShift]: open,
-            })}
-          >
-            <Toolbar className={classes.toolbar}>
-              <IconButton
-                color="inherit"
-                aria-label="open drawer"
-                onClick={handleDrawerOpen}
-                edge="start"
-                className={clsx(classes.menuButton, {
-                  [classes.hide]: open,
-                })}
-              >
-                <MenuIcon />
-              </IconButton>
-              <Typography variant="h6" noWrap className={classes.header}>
-                <Link to="/" style={{ color: 'inherit' }}>
-                  <Header />
-                </Link>
-              </Typography>
-              <Button color="inherit">LOGIN</Button>
-            </Toolbar>
-          </AppBar>
-          <Drawer
-            variant="permanent"
-            className={clsx(classes.drawer, {
-              [classes.drawerOpen]: open,
-              [classes.drawerClose]: !open,
-            })}
-            classes={{
-              paper: clsx({
-                [classes.drawerOpen]: open,
-                [classes.drawerClose]: !open,
-              }),
-            }}
-          >
-            <div className={classes.toolbar}>
-              <IconButton onClick={handleDrawerClose}>
-                {theme.direction === 'rtl' ? (
-                  <ChevronRightIcon />
-                ) : (
-                  <ChevronLeftIcon />
-                )}
-              </IconButton>
-            </div>
-            <Nav />
-          </Drawer>
+          <SidebarClosed />{' '}
         </>
       )}
       {children}
