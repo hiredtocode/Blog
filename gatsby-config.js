ee227c9dc0bcf8cbbfe4413f23ec2ec386a61d97
hiredtocode <jasonhan@kakao.com>
2021-03-07 19:23:11 +0900

    added templates folder and markdown file

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,11 +26,25 @@ module.exports = {
         },
       },
     },
-
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        // CommonMark mode (default: true)
+        commonmark: true,
+        // Footnotes mode (default: true)
+        footnotes: true,
+        // Pedantic mode (default: true)
+        pedantic: true,
+        // GitHub Flavored Markdown mode (default: true)
+        gfm: true,
+        // Plugins configs
+        plugins: [],
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `data`,
+        name: `markdown`,
         path: `${__dirname}/src/markdown/`,
         ignore: [`**/\\.*`], // ignore files starting with a dot
       },
