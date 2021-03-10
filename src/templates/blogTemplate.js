4c799a335ab38b24067444832f9d0fb9f86cea06
hiredtocode <jasonhan@kakao.com>
2021-03-10 13:44:19 +0900

    Aside content sidebar to the side with classes

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -1,25 +1,62 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import { graphql } from 'gatsby';
 import { MDXRenderer } from 'gatsby-plugin-mdx';
 import Layout from '../components/layout';
 import TableOfContents from '../components/tableofcontents';
+
+const useStyles = makeStyles((theme) => ({
+  aside: {
+    flexGrow: 0,
+    flexShrink: 100000,
+    flexBasis: `250px`,
+    position: `sticky`,
+    paddingTop: `23px`,
+    maxHeight: `calc(100vh - 148px)`,
+    marginLeft: `auto`,
+    overflow: `auto`,
+    [theme.breakpoints.up('md')]: {
+      display: 'block',
+    },
+    [theme.breakpoints.down('md')]: {
+      display: 'none',
+    },
+  },
+  article: {
+    display: `block`,
+    flexGrow: 1,
+    flexShrink: 1,
+    flexBasis: `686px`,
+    maxWidth: `686px`,
+  },
+  container: {
+    display: `flex`,
+    maxWidth: `1100px`,
+    paddingTop: `48px`,
+  },
+}));
 
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
 }) {
   const post = data.mdx; // data.mdx holds your post data
   const { frontmatter } = post;
+  const classes = useStyles();
   return (
     <Layout>
-      <div className="blog-post-container">
-        <div className="blog-post">
+      <div className={classes.container}>
+        <article className={classes.article}>
           <h1>{frontmatter.title}</h1>
           <MDXRenderer>{post.body}</MDXRenderer>
-          {post?.tableOfContents?.items && (
-            <TableOfContents items={post.tableOfContents.items} />
-          )}
           <h4>Last updated: {frontmatter.date}</h4>
-        </div>
+        </article>
+        <aside className={classes.aside}>
+          <nav>
+            {post?.tableOfContents?.items && (
+              <TableOfContents items={post.tableOfContents.items} />
+            )}
+          </nav>
+        </aside>
       </div>
     </Layout>
   );
