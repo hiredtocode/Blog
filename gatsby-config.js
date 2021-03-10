ad1ffb53654c751d7e0a8ba8e2154263b68ac5c4
hiredtocode <jasonhan@kakao.com>
2021-03-10 10:33:54 +0900

    File extensions option added under MDX plugin options to accept both .md .mdx

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,6 +29,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-mdx`,
       options: {
+        extensions: [`.md`, `.mdx`],
         gatsbyRemarkPlugins: [`gatsby-remark-slug`],
       },
     },
