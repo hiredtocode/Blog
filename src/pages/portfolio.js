dc6bb9b90de1462fa0133ca8f89e2dfc72a0eb16
hiredtocode <jasonhan@kakao.com>
2021-03-10 20:40:33 +0900

    Portfolio - wrote some content

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -4,5 +4,22 @@ import React from 'react';
 import Layout from '../components/layout';
 
 export default function Portfolio() {
-  return <Layout>Portfolio</Layout>;
+  return (
+    <Layout>
+      <h1>This website itself is an on-going project.</h1>
+      <p>
+        I can use a template and start a blog but I decided to make my own from
+        scratch using Gatsby and Material UI.
+      </p>
+      <p>
+        Currently I'm using Material UI's components such as the drawer and the
+        Appbar but I've learned a lot by implementing these components and
+        tweaking them to my needs.
+      </p>
+      <p>
+        This site is my first website that is built using Gatsby, Material UI
+        and React/JavaScript.
+      </p>
+    </Layout>
+  );
 }
