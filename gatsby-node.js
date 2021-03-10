9de3d0017b4faf67ac02107ffe6e1800eb10544c
hiredtocode <jasonhan@kakao.com>
2021-03-10 10:39:42 +0900

    Replaced allMarkdownRemark and MarkdownRemark to allMdx and mdx in gatsby-node file

--- gatsby-node.js
+++ gatsby-node.js
@@ -18,10 +18,7 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
 
   const result = await graphql(`
     {
-      allMarkdownRemark(
-        sort: { order: DESC, fields: [frontmatter___date] }
-        limit: 1000
-      ) {
+      allMdx(sort: { order: DESC, fields: [frontmatter___date] }, limit: 1000) {
         edges {
           node {
             frontmatter {
@@ -39,7 +36,7 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
     return;
   }
 
-  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+  result.data.allMdx.edges.forEach(({ node }) => {
     createPage({
       path: node.frontmatter.slug,
       component: blogPostTemplate,
