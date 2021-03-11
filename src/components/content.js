7225a7058c22e6c8a31554e46756bce5ad750805
hiredtocode <jasonhan@kakao.com>
2021-03-11 20:46:17 +0900

    Content - Added dummy left and right aside spacing

--- src/components/content.js
+++ src/components/content.js
@@ -2,28 +2,43 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Container } from '@material-ui/core';
 
-// const sidebarWidth = 250;
+const sidebarWidth = 240;
 const useStyles = makeStyles((theme) => ({
+  wrapper: {
+    display: `flex`,
+    flexDirection: `row`,
+  },
   content: {
-    display: 'flex',
-    flexDirection: 'column',
-    flexGrow: 1,
-    padding: theme.spacing(3),
-    paddingTop: `65px`,
+    width: `100%`,
+    paddingTop: `64px`,
     height: `auto`,
     minHeight: `calc(100vh - 95px)`,
+    [theme.breakpoints.up('sm')]: {
+      width: `calc(100vw - 175px)`,
+    },
+    [theme.breakpoints.up('md')]: {
+      width: `calc(100vw - 175px)`,
+    },
+  },
+  asideLeft: {
+    display: `block`,
+    flexGrow: 0,
+    minWidth: sidebarWidth,
     [theme.breakpoints.down('sm')]: {
-      paddingLeft: `10px`,
-      paddingRight: `10px`,
+      display: `none`,
+    },
+  },
+  asideRight: {
+    display: `block`,
+    flexGrow: 0,
+    minWidth: sidebarWidth,
+    [theme.breakpoints.down('sm')]: {
+      display: `none`,
     },
   },
   container: {
     display: `flex`,
     flexDirection: 'column',
-  },
-  paper: {
-    height: `auto`,
-    minHeight: `calc(100vh - 95px)`,
   },
 }));
 
@@ -31,10 +46,14 @@ export default function Content({ children }) {
   const classes = useStyles();
 
   return (
-    <main className={classes.content}>
-      <Container maxWidth="md" className={classes.container}>
-        {children}
-      </Container>
-    </main>
+    <div className={classes.wrapper}>
+      <nav className={classes.asideLeft} />
+      <main className={classes.content}>
+        <Container maxWidth="md" className={classes.container}>
+          {children}
+        </Container>
+      </main>
+      <nav className={classes.asideRight} />
+    </div>
   );
 }
