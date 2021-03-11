89ae3a926fea15e5be042afc1cbd5b2a786a6aee
hiredtocode <jasonhan@kakao.com>
2021-03-11 21:46:01 +0900

    Todo list added to index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,12 +9,14 @@ export default function Home() {
       <h1>Things I need to work on...</h1>
       <ul>
         <li>Dark mode toggle</li>
+        <li>Move table of contents to the aside right</li>
         <li>Make a todo page</li>
         <li>Search function</li>
         <li>Filter posts by tags</li>
         <li>Design the index page layout</li>
         <li>Design posts page layout</li>
         <li>Make a simple form in contacts page</li>
+        <li>Redesign sidebar menu list</li>
       </ul>
     </Layout>
   );
