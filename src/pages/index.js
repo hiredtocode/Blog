0b66cee2d2c85562ae49aafacb2e53cee3a3b15e
hiredtocode <jasonhan@kakao.com>
2021-03-11 21:42:37 +0900

    Todo list added to index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,5 +4,18 @@ import React from 'react';
 import Layout from '../components/layout';
 
 export default function Home() {
-  return <Layout>Home</Layout>;
+  return (
+    <Layout>
+      <h1>Things I need to work on...</h1>
+      <ul>
+        <li>Dark mode toggle</li>
+        <li>Make a todo page</li>
+        <li>Search function</li>
+        <li>Filter posts by tags</li>
+        <li>Design the index page layout</li>
+        <li>Design posts page layout</li>
+        <li>Make a simple form in contacts page</li>
+      </ul>
+    </Layout>
+  );
 }
