deea710a7f928a17b1ae39a330a789de0ab2621c
hiredtocode <jasonhan@kakao.com>
2021-03-12 12:20:29 +0900

    Menu text aligned to the right

--- src/components/nav.js
+++ src/components/nav.js
@@ -5,6 +5,7 @@ import {
   ListItem,
   ListItemIcon,
   ListItemText,
+  makeStyles,
 } from '@material-ui/core';
 import { Link } from 'gatsby-theme-material-ui';
 import HomeIcon from '@material-ui/icons/Home';
@@ -13,58 +14,64 @@ import LibraryBooksIcon from '@material-ui/icons/LibraryBooks';
 import ContactMailIcon from '@material-ui/icons/ContactMail';
 import PostAddIcon from '@material-ui/icons/PostAdd';
 
+const useStyles = makeStyles(() => ({
+  listText: {
+    textAlignLast: `end`,
+    paddingRight: `10px`,
+  },
+}));
+
 export default function Nav() {
-  const style = {
+  const activeLink = {
     color: `red`,
   };
+  const classes = useStyles();
 
   return (
-    // <ThemeProvider theme={theme}>
-    // <nav className={classes.drawer}>
     // TODO Need to make the icons change colors too so that when the side menu is minimized users can see where they're at
     // TODO tooltip when side menu is collapsed
     // TODO clicked menu background color change (Mui selected-menus)
     <div>
       <Divider />
       <List component="nav">
-        <Link to="/" activeStyle={style}>
+        <Link to="/" activeStyle={activeLink}>
           <ListItem button>
             <ListItemIcon>
-              <HomeIcon color="primary" />
+              <HomeIcon />
             </ListItemIcon>
-            <ListItemText primary="Home" />
+            <ListItemText primary="Home" className={classes.listText} />
           </ListItem>
         </Link>
-        <Link to="/about" activeStyle={style}>
+        <Link to="/about" activeStyle={activeLink}>
           <ListItem button>
             <ListItemIcon>
-              <InfoIcon color="primary" />
+              <InfoIcon />
             </ListItemIcon>
-            <ListItemText primary="About" />
+            <ListItemText primary="About" className={classes.listText} />
           </ListItem>
         </Link>
-        <Link to="/posts" activeStyle={style}>
+        <Link to="/posts" activeStyle={activeLink}>
           <ListItem button>
             <ListItemIcon>
-              <PostAddIcon color="primary" />
+              <PostAddIcon />
             </ListItemIcon>
-            <ListItemText primary="Posts" />
+            <ListItemText primary="Posts" className={classes.listText} />
           </ListItem>
         </Link>
-        <Link to="/contact" activeStyle={style}>
+        <Link to="/contact" activeStyle={activeLink}>
           <ListItem button>
             <ListItemIcon>
-              <ContactMailIcon color="primary" />
+              <ContactMailIcon />
             </ListItemIcon>
-            <ListItemText primary="Contact" />
+            <ListItemText primary="Contact" className={classes.listText} />
           </ListItem>
         </Link>
-        <Link to="/portfolio" activeStyle={style}>
+        <Link to="/portfolio" activeStyle={activeLink}>
           <ListItem button>
             <ListItemIcon>
-              <LibraryBooksIcon color="primary" />
+              <LibraryBooksIcon />
             </ListItemIcon>
-            <ListItemText primary="Portfolio" />
+            <ListItemText primary="Portfolio" className={classes.listText} />
           </ListItem>
         </Link>
       </List>
