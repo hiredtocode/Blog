7e27e83b02a4e0851fc86d87453c07b511b18518
hiredtocode <jasonhan@kakao.com>
2021-03-16 18:41:23 +0900

    Global link color added

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import { CssBaseline } from '@material-ui/core';
+import { CssBaseline, useMediaQuery } from '@material-ui/core';
 import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import Content from './content';
 import Footer from './footer';
 import Sidebar from './sidebar';
 
 const theme = createMuiTheme({
-  overrides: {
-    MuiCssBaseline: {
-      '@global': {
-        html: {
-          WebkitFontSmoothing: 'auto',
-          height: `100%`,
-          scrollBehavior: `smooth`,
-          scrollPaddingTop: `80px`,
-        },
-        a: {
-          textDecoration: `none`,
-        },
-      },
+  palette: {
+    primary: {
+      main: '#262626',
     },
+    secondary: {
+      main: '#ff0303',
+    },
+    type: `light`,
   },
   props: {
     MuiButtonBase: {
@@ -30,7 +24,49 @@ const theme = createMuiTheme({
     },
     MuiContainer: {},
   },
+  overrides: {
+    MuiCssBaseline: {
+      '@global': {
+        html: {
+          WebkitFontSmoothing: 'auto',
+          height: `100%`,
+          scrollBehavior: `smooth`,
+          scrollPaddingTop: `80px`,
+        },
+        a: {
+          color: `#262626`,
+          textDecoration: `none`,
+
+          '&:visited': {
+            color: `#767676`,
+          },
+          '&:hover': {
+            color: `#4600a7`,
+          },
+          '&.active': {
+            color: `#444444`,
+            fontWeight: `bold`,
+          },
+        },
+      },
+    },
+  },
 });
+
+// Set dark mode automatically
+// function App() {
+//  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+//  const theme = React.useMemo(
+//    () =>
+//      createMuiTheme({
+//        palette: {
+//          type: prefersDarkMode ? 'dark' : 'light',
+//        },
+//      }),
+//    [prefersDarkMode]
+//  );
+// }
 
 export default function Layout({ children }) {
   return (
