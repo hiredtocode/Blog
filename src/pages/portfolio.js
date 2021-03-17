d48346f7ae590789f7adac516cc748f0efc274ad
hiredtocode <jasonhan@kakao.com>
2021-03-17 21:19:22 +0900

    Added portfolio item - Nama Sushi

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -2,11 +2,13 @@
 
 import React from 'react';
 import Layout from '../components/layout';
+import NamaSushi from '../components/namasushi';
 
 export default function Portfolio() {
   return (
     <Layout>
-      <h2>This website itself is an on-going project.</h2>
+      <NamaSushi />
+      <p>This website itself is an on-going project.</p>
       <p>
         I could probably use a template and start a blog but I decided to make my own from scratch using Gatsby and
         Material UI.
