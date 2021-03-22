bde4730f8db49b150f7624175297825c046a4032
hiredtocode <jasonhan@kakao.com>
2021-03-22 15:07:33 +0900

    Added expand function for more information

--- src/components/namasushi.js
+++ src/components/namasushi.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import { makeStyles, CardActionArea, CardActions } from '@material-ui/core';
+import { makeStyles, CardActionArea, CardActions, Collapse, IconButton } from '@material-ui/core';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
+import clsx from 'clsx';
+import { red } from '@material-ui/core/colors';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import NamaSushiImage from '../../static/images/cards/namasushi.jpg';
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     maxWidth: 345,
   },
@@ -19,10 +22,28 @@ const useStyles = makeStyles(() => ({
       transition: ['800ms'],
     },
   },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  avatar: {
+    backgroundColor: red[500],
+  },
 }));
 
 export default function NamaSushi() {
   const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
   return (
     <Card className={classes.root}>
       <a href="https://namasushi.ca" target="_" rel="noopener">
@@ -32,14 +53,32 @@ export default function NamaSushi() {
           <CardMedia component="img" className={classes.media} src={NamaSushiImage} title="Nama Sushi" />
           <CardContent>
             <Typography variant="body2" color="textSecondary" component="p">
-              This website was built using Wordpress for one of my client in 2016. <br /> <br />
-              I've made a few websites like these in the past but they all went out of business and the websites don't
-              exist anymore...
+              This website was built using Wordpress for one of my client in 2016.
             </Typography>
           </CardContent>
         </CardActionArea>
         <CardActions />
       </a>
+      <CardActions disableSpacing>
+        <IconButton
+          className={clsx(classes.expand, {
+            [classes.expandOpen]: expanded,
+          })}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </IconButton>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>
+            I've made a few websites like these in the past but they all went out of business and the websites don't
+            exist anymore...
+          </Typography>
+        </CardContent>
+      </Collapse>
     </Card>
   );
 }
--- src/components/webdev.js
+++ src/components/webdev.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import { makeStyles, CardActionArea, CardActions } from '@material-ui/core';
+import { makeStyles, CardActionArea, CardActions, Collapse, IconButton } from '@material-ui/core';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
+import clsx from 'clsx';
+import { red } from '@material-ui/core/colors';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import WebDev from '../../static/images/cards/webdevmadness.jpg';
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     maxWidth: 345,
   },
@@ -19,10 +22,28 @@ const useStyles = makeStyles(() => ({
       transition: ['800ms'],
     },
   },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  avatar: {
+    backgroundColor: red[500],
+  },
 }));
 
 export default function WebDevMadness() {
   const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
   return (
     <Card className={classes.root}>
       <a href="https://jasonhan.netlify.app" target="_" rel="noopener">
@@ -32,14 +53,32 @@ export default function WebDevMadness() {
           <CardMedia component="img" className={classes.media} src={WebDev} title="Web Dev Madness" />
           <CardContent>
             <Typography variant="body2" color="textSecondary" component="p">
-              This website is my latest project which is built by using Gatsby and deployed to Netlify. Instead of using
-              a pre-made template, I decided to make it from scratch with help of Material UI. I will be using this
-              website as a blog as well as a portfolio.
+              This website is my latest project which is built by using Gatsby and deployed to Netlify.
             </Typography>
           </CardContent>
         </CardActionArea>
         <CardActions />
       </a>
+      <CardActions disableSpacing>
+        <IconButton
+          className={clsx(classes.expand, {
+            [classes.expandOpen]: expanded,
+          })}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </IconButton>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>
+            Instead of using a pre-made template, I decided to make it from scratch with help of Material UI. I will be
+            using this website as a blog as well as a portfolio.
+          </Typography>
+        </CardContent>
+      </Collapse>
     </Card>
   );
 }
