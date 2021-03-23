cf06621e832917e1f9fb59af747c08a1a83252d9
hiredtocode <jasonhan@kakao.com>
2021-03-24 00:15:31 +0900

    Index - Korean intro added

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,17 @@ import React from 'react';
 export default function Home() {
   return (
     <>
-      <h2>Things I need to work on:</h2>
+      <h2>안녕하세요, 저는 Jason Han 입니다.</h2>
+      <p>이 사이트는 현제 작업 중입니다.</p>
+      <p>현재 한/영 번역이 되는 기능을 만들고 있습니다.</p>
+      <p>
+        아직 한글보다 영어로 대부분 작업을 해 놔서 죄송합니다... <br /> 빠른 시일 안에 한글로도 볼수 있는
+        포트폴리오/블로그를 완성 하겠습니다.
+      </p>
+      <br />
+      <h2>Todo list:</h2>
       <ul>
+        <li>English/Korean</li>
         <li>Dark mode toggle</li>
         <li>Optimize Gatsby build time</li>
         <li>Reduce build time</li>
@@ -19,7 +28,6 @@ export default function Home() {
         <li>Design posts page layout</li>
         <li>Make a simple form in contacts page</li>
         <li>Redesign sidebar menu list</li>
-        <li>English/Korean</li>
       </ul>
     </>
   );
