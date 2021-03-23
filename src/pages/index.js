ac9e7b69a720bc248fbd0de8385b2db942d35499
hiredtocode <jasonhan@kakao.com>
2021-03-24 00:26:46 +0900

    Minor update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,21 @@
 /** @format */
 
+import { Divider } from '@material-ui/core';
 import React from 'react';
 
 export default function Home() {
   return (
     <>
-      <h2>안녕하세요, 저는 Jason Han 입니다.</h2>
-      <p>이 사이트는 현제 작업 중입니다.</p>
-      <p>현재 한/영 번역이 되는 기능을 만들고 있습니다.</p>
+      <h2>안녕하세요, 저는 한형석 입니다.</h2>
+      <p>이 사이트는 현재 작업 중입니다.</p>
+      <p>한/영 번역이 되는 기능을 만들고 있습니다. 기다려 주셔서 감사 드립니다.</p>
       <p>
         아직 한글보다 영어로 대부분 작업을 해 놔서 죄송합니다... <br /> 빠른 시일 안에 한글로도 볼수 있는
-        포트폴리오/블로그를 완성 하겠습니다.
+        포트폴리오/블로그를 완성을 하겠습니다.
       </p>
+      <p>3월 23일 2021년 - 업데이트 됨.</p>
+      <br />
+      <Divider />
       <br />
       <h2>Todo list:</h2>
       <ul>
